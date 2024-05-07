e6efe2247163098c18bb79f66f86071beac21f58
Giovanne Fernandes <3518@ucdb.br>
2024-05-07 08:53:46 -0400

    Add project files.

---
+++ src/UCDB.BlipIntegrationApi.Api/Controllers/AcademicoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UCDB.BlipIntegrationApi.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AcademicoController : ControllerBase
+{
+    [HttpGet(Name = "GetAcademico")]
+    public Academico Get()
+    {
+        return new Academico
+        {
+            Nome = "Academico",
+            Matricula = "123456",
+            Curso = "Tads",
+            Periodo = "2024A"
+        };
+    }
+}
+
+public class Academico
+{
+    public string Nome { get; set; }
+    public string Matricula { get; set; }
+    public string Curso { get; set; }
+    public string Periodo { get; set; }
+}
